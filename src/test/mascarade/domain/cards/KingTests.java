47f7e194a2a2236b5a32663fc39038acb2651fd7
Bartosz Wozniak <bartosz.wozniak@hotmail.com>
2018-08-05 13:28:22 +0200

    Test folder

---
+++ src/test/mascarade/domain/cards/KingTests.java
@@ -0,0 +1,14 @@
+package mascarade.domain.cards;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KingTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
